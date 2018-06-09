0d47336594f4f86e7c1c3356f716a8cd6faa145f
Aakash Das <aakashsjr@gmail.com>
2018-06-10 04:16:37 +0530

    delau added

--- bot/tasks.py
+++ bot/tasks.py
@@ -13,26 +13,25 @@ def process(driver, products, find_out_of_stock=False):
     url = "http://www.dropship-clothes.com/"
 
     for product_code in products:
-        time.sleep(1)
-        driver.get(url)
+        start = time.time()
+        delay = 0
         print("Looking into item {}".format(product_code))
-        s_time = time.time()
-        e_time = time.time()
         search_box = None
-        while e_time - s_time < 10:
+        while True:
             try:
+                time.sleep(delay)
+                driver.get(url)
                 search_box = driver.find_element_by_class_name('search_input')
             except:
+                delay += 1
                 driver.save_screenshot("/tmp/{}.png".format(time.time()))
                 print("Refinding search box")
             else:
                 break
-        start = time.time()
         search_box.click()
         search_box.clear()
         search_box.send_keys(product_code)
         search_box.send_keys(Keys.ENTER)
-        time.sleep(0.2)
         items = driver.find_elements_by_class_name('pic')
         if len(items):
             item = items[0]
