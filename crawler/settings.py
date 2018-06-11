a6c164051ab6f738040eeb8b4089086ea3bc81f8
Aakash Das <aakashsjr@gmail.com>
2018-06-11 17:44:27 +0530

    d

--- crawler/settings.py
+++ crawler/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kzzzb5d63x#t2wlxazwnuvvixyr6mfcacbs^u$pk%p-t#j75-&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
